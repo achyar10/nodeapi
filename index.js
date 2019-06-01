bd32701498e2e955653dd67dc0500e84c5ae3eac
achyar10 <achyar.anshorie@gmail.com>
2019-06-01 21:53:12 +0700

    ok

---
+++ index.js
@@ -0,0 +1,44 @@
+// import library
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+// import route
+import route from './route'
+
+const app = express()
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Header', 'Origin, X-reqed-With, Content-Type, Accept')
+  next();
+})
+
+// set bodyParser sebagai middleware yang akan memparsing body req
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// connect ke database
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/todo', {
+  useNewUrlParser: true
+})
+
+// set route
+app.use('/api', route)
+
+app.get('/', (req, res) => {
+  return res.end('API Working')
+})
+
+// jika route tidak ditemukan
+app.use((req, res, next) => {
+  res.status(404).send('<h2 align=center>Page not found!</h2>')
+})
+
+// start server
+const port = 3000
+app.listen(port, () => {
+  console.log(`App Server Listening at ${port}`)
+})
